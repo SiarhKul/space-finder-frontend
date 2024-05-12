efcec6e953b40d6d9745e4242526c22bfb9a5570
siarhkul <siarhkul@gmail.com>
2024-05-12 16:57:01 +0200

    feat: Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userName] = useState<string | undefined>(undefined);
+
+  const router = createBrowserRouter([
+    {
+      element: (
+        <>
+          <NavBar userName={userName}/>
+          <Outlet />
+        </>
+      ),
+      children:[
+        {
+          path: "/",
+          element: <div>Hello world!</div>,
+        },
+        {
+          path: "/login",
+          element: <div>Login page</div>,
+        },
+        {
+          path: "/profile",
+          element: <div>Profile page</div>,
+        },
+        {
+          path: "/createSpace",
+          element: <div>Create space page</div>,
+        },
+        {
+          path: "/spaces",
+          element: <div>Spaces page </div>,
+        },
+      ]
+    },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="wrapper">
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
