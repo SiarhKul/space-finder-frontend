5ab1a529a3f68340b6e3b792a5b4b5da4ae3d46a
siarhkul <siarhkul@gmail.com>
2024-05-15 13:46:14 +0200

    feat: Send image to AWS

--- src/services/DataService.tsx
+++ src/services/DataService.tsx
@@ -1,21 +1,53 @@
 import {AuthService} from "./AuthService.ts";
+import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import {DataStack} from '../../../space-finder/outputs.json'
 
 export class DataService {
 
-  private authService: AuthService;
+    private authService: AuthService;
+    private s3Client: S3Client | undefined;
+    private awsRegion = 'eu-central-1'
 
-  constructor(authService: AuthService) {
-    this.authService = authService
-  }
+    constructor(authService: AuthService) {
+        this.authService = authService
+    }
 
-  public async createSpace(name: string, location: string, photo?: File) {
-    const credantions = await this.authService.getTemporaryCredentials()
-    console.log("=>(DataService.tsx:13) credantions", credantions);
+    public async createSpace(name: string,
+                             location: string,
+                             photo?: File) {
+        if (photo) {
+            const uploadUrl = await this.uploadPublicFile(photo)
+            console.log("=>(DataService.tsx:21) uploadUrl", uploadUrl);
+        }
 
-    return '123'
-  }
+        return '123'
+    }
 
-  public isAuthorized() {
-    return true;
-  }
+
+    private async uploadPublicFile(file: File) {
+        const credantions = await this.authService.getTemporaryCredentials()
+
+        if (!this.s3Client) {
+            this.s3Client = new S3Client({
+                credentials: credantions as any,
+                region: this.awsRegion
+            })
+        }
+
+        const command = new PutObjectCommand({
+            Bucket: DataStack.Space,
+            Key: file.name,
+            ACL: "public-read",
+            Body: file
+        })
+
+        await this.s3Client.send(command)
+
+        return `https://${command.input.Bucket}.s3.${this.awsRegion}.amazonaws.com/${command.input.Key}`
+    }
+
+
+    public isAuthorized() {
+        return true;
+    }
 }
